def111205ddf3066a3900398ce78c7a6815b6140
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-04-10 01:44:06 +0600

    Add CLI11 to Hessian example

--- example/hessian.cpp
+++ example/hessian.cpp
@@ -5,6 +5,8 @@
 #include <boost/gil/extension/io/png.hpp>
 #include <boost/gil/typedefs.hpp>
 
+#include <CLI11.hpp>
+
 #include <iostream>
 #include <limits>
 
@@ -15,11 +17,33 @@ namespace gil = boost::gil;
 
 int main(int argc, char* argv[])
 {
-    gil::rgb8_image_t input;
-    gil::read_image(argv[1], input, gil::png_tag{});
+    std::string input_file;
+    std::string determinants_map_file;
+    std::string traces_map_file;
+    std::string output_file;
 
-    std::int32_t dets_threshold = std::stol(argv[5]);
-    std::int32_t traces_threshold = std::stol(argv[6]);
+    std::int32_t dets_threshold;
+    std::int32_t traces_threshold;
+
+    CLI::App app{"Demonstration of Hessian affine region detector - finding corners"};
+    app.add_option("i,--input", input_file, "PNG file with RGB colorspace")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("dmf", determinants_map_file, "determinants map file will be a grayscale PNG image with determinant of Hessian as pixels")
+        ->required();
+    app.add_option("tmf", traces_map_file, "traces map file will be a grayscale PNG image with trace of Hessian as pixels")
+        ->required();
+    app.add_option("o,--output", output_file, "Output file that will contain green markers where Hessian corner detector tested positive")
+        ->required();
+    app.add_option("dt", dets_threshold, "Threshold for determinants response")
+        ->required();
+    app.add_option("tt", traces_threshold, "Threshold for traces response")
+        ->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    gil::rgb8_image_t input;
+    gil::read_image(input_file, input, gil::png_tag{});
 
     gil::gray8_image_t gray(input.dimensions());
     gil::copy_and_convert_pixels(gil::view(input), gil::view(gray));
@@ -47,7 +71,7 @@ int main(int argc, char* argv[])
 
     std::cout << "Gradient range: " << blaze::max(hessian_result.determinants) << ' ' << blaze::min(hessian_result.determinants) << '\n'
               << "Final gray image range: " << static_cast<int>(blaze::max(image)) << ' ' << static_cast<int>(blaze::min(image)) << '\n';
-    gil::write_view(argv[2], gil::view(determinants_map), gil::png_tag{});
-    gil::write_view(argv[3], gil::view(traces_map), gil::png_tag{});
-    gil::write_view(argv[4], gil::view(input), gil::png_tag{});
+    gil::write_view(determinants_map_file, gil::view(determinants_map), gil::png_tag{});
+    gil::write_view(traces_map_file, gil::view(traces_map), gil::png_tag{});
+    gil::write_view(output_file, gil::view(input), gil::png_tag{});
 }
