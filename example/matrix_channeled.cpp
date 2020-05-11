e80cfe7efc295b7c95fb5286e41978a5f8d434e9
Olzhas <anonymous.from.applecity@gmail.com>
2020-05-11 16:29:41 +0600

    Implement remap_to_channeled

--- example/matrix_channeled.cpp
+++ example/matrix_channeled.cpp
@@ -13,16 +13,15 @@ int main()
     auto view = gil::view(image);
 
     view(0, 0) = gil::rgb8_pixel_t(0, 0, 0);
-    // view(4, 4) = gil::rgb8_pixel_t(0, 0, 0);
+    view(4, 4) = gil::rgb8_pixel_t(0, 0, 0);
 
     auto matrix = flash::to_matrix_channeled(view);
     std::cout << matrix.rows() << ' ' << matrix.columns() << '\n';
 
     for (std::size_t i = 0; i < matrix.rows(); ++i) {
         for (std::size_t j = 0; j < matrix.columns(); ++j) {
-            std::cout << "m[" << i << "][" << j << "] = ("
-                      << static_cast<int>(matrix(i, j)[0]) << ' '
-                      << static_cast<int>(matrix(i, j)[1]) << ' '
+            std::cout << "m[" << i << "][" << j << "] = (" << static_cast<int>(matrix(i, j)[0])
+                      << ' ' << static_cast<int>(matrix(i, j)[1]) << ' '
                       << static_cast<int>(matrix(i, j)[2]) << ") ";
         }
         std::cout << '\n';
@@ -31,13 +30,13 @@ int main()
     std::cout << "vector to pixel:\n";
     auto v = blaze::StaticVector<unsigned char, 3>{3, 4, 5};
     auto pixel = flash::vector_to_pixel<gil::rgb8_pixel_t>(v);
-    std::cout << static_cast<int>(pixel[0]) << ' ' << static_cast<int>(pixel[1])
-              << ' ' << static_cast<int>(pixel[2]) << '\n';
+    std::cout << static_cast<int>(pixel[0]) << ' ' << static_cast<int>(pixel[1]) << ' '
+              << static_cast<int>(pixel[2]) << '\n';
 
     std::cout << "matrix to image:\n";
     auto converted_image = flash::to_image<gil::rgb8_image_t>(matrix);
-    std::cout << "width: " << converted_image.width()
-              << " height: " << converted_image.height() << '\n';
+    std::cout << "width: " << converted_image.width() << " height: " << converted_image.height()
+              << '\n';
     auto converted_view = gil::view(converted_image);
     for (flash::signed_size i = 0; i < converted_image.height(); ++i) {
         for (flash::signed_size j = 0; j < converted_image.width(); ++j) {
@@ -45,6 +44,29 @@ int main()
                       << static_cast<int>(converted_view(i, j)[0]) << ' '
                       << static_cast<int>(converted_view(i, j)[1]) << ' '
                       << static_cast<int>(converted_view(i, j)[2]) << ") ";
+        }
+        std::cout << '\n';
+    }
+
+    blaze::DynamicMatrix<blaze::StaticVector<double, 3>> mat(matrix);
+    mat = mat % mat % mat % mat % mat;
+    auto remapped = flash::remap_to_channeled<unsigned char>(mat);
+
+    for (std::size_t i = 0; i < mat.rows(); ++i) {
+        for (std::size_t j = 0; j < mat.columns(); ++j) {
+            std::cout << "source[" << i << "][" << j << "] = (" << static_cast<int>(mat(i, j)[0])
+                      << ' ' << static_cast<int>(mat(i, j)[1]) << ' '
+                      << static_cast<int>(mat(i, j)[2]) << ") ";
+        }
+        std::cout << '\n';
+    }
+
+    for (std::size_t i = 0; i < remapped.rows(); ++i) {
+        for (std::size_t j = 0; j < remapped.columns(); ++j) {
+            std::cout << "remapped[" << i << "][" << j << "] = ("
+                      << static_cast<int>(remapped(i, j)[0]) << ' '
+                      << static_cast<int>(remapped(i, j)[1]) << ' '
+                      << static_cast<int>(remapped(i, j)[2]) << ") ";
         }
         std::cout << '\n';
     }
--- include/flash/core.hpp
+++ include/flash/core.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <algorithm>
 #include <blaze/Blaze.h>
+#include <blaze/math/expressions/Forward.h>
+#include <blaze/math/typetraits/IsDenseVector.h>
+#include <blaze/math/typetraits/IsStatic.h>
+#include <blaze/math/typetraits/UnderlyingElement.h>
 #include <boost/gil/algorithm.hpp>
 #include <boost/gil/image.hpp>
 #include <boost/gil/image_view.hpp>
@@ -12,6 +17,34 @@
 #include <utility>
 
 #include <iostream>
+
+namespace std
+{
+template <typename U, std::size_t N>
+struct numeric_limits<blaze::StaticVector<U, N>> {
+    static constexpr auto min()
+    {
+        auto v = blaze::StaticVector<U, N>{};
+        std::fill(v.begin(), v.end(), std::numeric_limits<U>::min());
+
+        return v;
+    }
+
+    static constexpr auto max()
+    {
+        auto v = blaze::StaticVector<U, N>{};
+        std::fill(v.begin(), v.end(), std::numeric_limits<U>::max());
+
+        return v;
+    }
+};
+
+template <typename T, typename U, std::size_t N>
+struct common_type<blaze::StaticVector<T, N>, blaze::StaticVector<U, N>> {
+    using type = blaze::StaticVector<std::common_type<T, U>, N>;
+};
+
+} // namespace std
 
 namespace flash
 {
@@ -28,12 +61,10 @@ using image_matrix = blaze::CustomMatrix<T, blaze::unaligned, blaze::unpadded>;
 namespace detail
 {
 template <typename PixelType, std::size_t... indices>
-auto pixel_to_vector_impl(const PixelType& pixel,
-                          std::integer_sequence<std::size_t, indices...>)
+auto pixel_to_vector_impl(const PixelType& pixel, std::integer_sequence<std::size_t, indices...>)
 {
     using ChannelType = typename boost::gil::channel_type<PixelType>::type;
-    return blaze::StaticVector<ChannelType, sizeof...(indices)>{
-        pixel[indices]...};
+    return blaze::StaticVector<ChannelType, sizeof...(indices)>{pixel[indices]...};
 }
 } // namespace detail
 
@@ -41,15 +72,14 @@ template <typename PixelType>
 auto pixel_to_vector(const PixelType& pixel)
 {
     return detail::pixel_to_vector_impl(
-        pixel,
-        std::make_index_sequence<boost::gil::num_channels<PixelType>{}>{});
+        pixel, std::make_index_sequence<boost::gil::num_channels<PixelType>{}>{});
 }
 
 template <typename View>
 auto to_matrix_channeled(View view)
 {
-    return blaze::evaluate(blaze::generate(
-        view.height(), view.width(), [&view](std::size_t i, std::size_t j) {
+    return blaze::evaluate(
+        blaze::generate(view.height(), view.width(), [&view](std::size_t i, std::size_t j) {
             return pixel_to_vector(view(j, i));
         }));
 }
@@ -57,13 +87,10 @@ auto to_matrix_channeled(View view)
 template <typename GrayView>
 auto to_matrix(GrayView source)
 {
-    using value_type =
-        remove_cvref_t<decltype(std::declval<typename GrayView::value_type>()
-                                    .at(std::integral_constant<int, 0>{}))>;
+    using value_type = remove_cvref_t<decltype(
+        std::declval<typename GrayView::value_type>().at(std::integral_constant<int, 0>{}))>;
     return image_matrix<value_type>(
-        reinterpret_cast<value_type*>(&source(0, 0)),
-        source.height(),
-        source.width());
+        reinterpret_cast<value_type*>(&source(0, 0)), source.height(), source.width());
 }
 
 // perform linear mapping from source range to destination range
@@ -72,21 +99,18 @@ auto to_matrix(GrayView source)
 // to destination range
 template <typename U, typename SourceMatrix, typename T>
 auto remap_to(const SourceMatrix& source, T src_min, T src_max,
-              U dst_min = std::numeric_limits<U>::min(),
-              U dst_max = std::numeric_limits<U>::max())
+              U dst_min = std::numeric_limits<U>::min(), U dst_max = std::numeric_limits<U>::max())
 {
     // ensure that dst_max - dst_min will not overflow
-    return blaze::map(
-        source,
-        [src_min,
-         dst_min,
-         src_length = src_max - src_min,
-         dst_length = static_cast<T>(dst_max) - dst_min](T value) {
-            // attempt to avoid integral inaccuracy
-            return static_cast<U>(dst_min +
-                                  static_cast<double>(value - src_min) /
-                                      src_length * dst_length);
-        });
+    return blaze::map(source,
+                      [src_min,
+                       dst_min,
+                       src_length = src_max - src_min,
+                       dst_length = static_cast<T>(dst_max) - dst_min](T value) {
+                          // attempt to avoid integral inaccuracy
+                          return static_cast<U>(dst_min + static_cast<double>(value - src_min) /
+                                                              src_length * dst_length);
+                      });
 }
 
 template <typename U, typename SourceMatrix>
@@ -95,8 +119,76 @@ auto remap_to(const SourceMatrix& source)
     return remap_to<U>(source, blaze::min(source), blaze::max(source));
 }
 
-boost::gil::gray8_image_t
-to_gray8_image(const blaze::DynamicMatrix<std::uint8_t>& source)
+// remap larger range into smaller one
+template <typename U, typename MT, bool SO>
+auto remap_to_channeled(const blaze::DenseMatrix<MT, SO>& source,
+                        U dst_min = std::numeric_limits<U>::min(),
+                        U dst_max = std::numeric_limits<U>::max())
+{
+    using source_vector_type = blaze::UnderlyingElement_t<MT>;
+    constexpr auto vector_size = source_vector_type::size();
+    using element_type = blaze::UnderlyingElement_t<source_vector_type>;
+    static_assert(blaze::IsStatic_v<source_vector_type> &&
+                  blaze::IsDenseVector_v<source_vector_type>);
+    auto min_reducer = [](source_vector_type prev, const source_vector_type& current) {
+        for (std::size_t i = 0; i < prev.size(); ++i) {
+            if (prev[i] > current[i]) {
+                prev[i] = current[i];
+            }
+        }
+
+        return prev;
+    };
+    auto src_min_elems = blaze::reduce(source, min_reducer);
+
+    auto max_reducer = [](source_vector_type prev, const source_vector_type& current) {
+        for (std::size_t i = 0; i < prev.size(); ++i) {
+            if (prev[i] < current[i]) {
+                prev[i] = current[i];
+            }
+        }
+
+        return prev;
+    };
+    auto src_max_elems = blaze::reduce(source, max_reducer);
+
+    using result_vector_type = blaze::StaticVector<U, source_vector_type::size()>;
+
+    // pick according to std::common_type rules to make
+    // more resistant to overflow/underflow and accuracy loss
+    using proxy_type = std::common_type_t<element_type, U>;
+    using proxy_vector = blaze::StaticVector<proxy_type, source_vector_type::size()>;
+
+    result_vector_type dst_min_elems;
+    std::fill(dst_min_elems.begin(), dst_min_elems.end(), dst_min);
+    result_vector_type dst_max_elems;
+    std::fill(dst_max_elems.begin(), dst_max_elems.end(), dst_max);
+
+    // use proxy range to
+    proxy_vector src_range_length = src_max_elems - src_min_elems + 1;
+    proxy_vector dst_range_length = dst_max_elems - dst_min_elems + 1;
+
+    return blaze::evaluate(blaze::map(
+        (~source),
+        [src_min_elems, dst_min_elems, src_range_length, dst_range_length, vector_size](
+            const source_vector_type& elem) {
+            result_vector_type result{};
+            for (std::size_t i = 0; i < vector_size; ++i) {
+                result[i] = dst_min_elems[i] + // substract 1 because ranges usually start from 0
+                            (static_cast<double>(elem[i] -
+                                                 src_min_elems[i]) / // cast to double to make more
+                                                                     // resistant to accuracy loss
+                             src_range_length[i] *
+                             dst_range_length[i]);
+                std::cout << static_cast<int>(result[i]) << ' ' << elem[i] << ' '
+                          << src_range_length[i] << ' ' << dst_range_length[i] << '\n';
+            }
+
+            return result;
+        }));
+}
+
+inline boost::gil::gray8_image_t to_gray8_image(const blaze::DynamicMatrix<std::uint8_t>& source)
 {
     boost::gil::gray8_image_t result(source.columns(), source.rows());
     auto matrix_view = to_matrix(boost::gil::view(result));
@@ -116,8 +208,7 @@ auto vector_to_pixel(const blaze::DenseVector<VT, TransposeFlag>& vector)
     return pixel;
 }
 
-template <typename ImageType,
-          typename PixelType = typename ImageType::value_type, typename MT>
+template <typename ImageType, typename PixelType = typename ImageType::value_type, typename MT>
 ImageType to_image(const blaze::DenseMatrix<MT, blaze::rowMajor>& data)
 {
     ImageType image((~data).rows(), (~data).columns());
@@ -133,37 +224,30 @@ ImageType to_image(const blaze::DenseMatrix<MT, blaze::rowMajor>& data)
 }
 
 template <typename T, typename U>
-blaze::DynamicMatrix<T> pad(const blaze::DynamicMatrix<T>& source,
-                            std::size_t pad_count, const U& padding_value)
+blaze::DynamicMatrix<T> pad(const blaze::DynamicMatrix<T>& source, std::size_t pad_count,
+                            const U& padding_value)
 {
     static_assert(std::is_convertible_v<T, U>);
 
-    blaze::DynamicMatrix<T> result(source.rows() + pad_count * 2,
-                                   source.columns() + pad_count * 2);
+    blaze::DynamicMatrix<T> result(source.rows() + pad_count * 2, source.columns() + pad_count * 2);
 
     auto full_resulting_width = source.columns() + pad_count * 2;
     // first pad_count rows
-    blaze::submatrix(result, 0, 0, pad_count, full_resulting_width) =
-        padding_value;
+    blaze::submatrix(result, 0, 0, pad_count, full_resulting_width) = padding_value;
     // last pad_count rows
-    blaze::submatrix(
-        result, source.rows(), 0, pad_count, full_resulting_width) =
-        padding_value;
+    blaze::submatrix(result, source.rows(), 0, pad_count, full_resulting_width) = padding_value;
 
     auto vertical_block_height = source.rows();
     // left pad_count columns, do note that top pad_count rows are already
     // filled
-    blaze::submatrix(result, pad_count, 0, vertical_block_height, pad_count) =
-        padding_value;
+    blaze::submatrix(result, pad_count, 0, vertical_block_height, pad_count) = padding_value;
     // right pad_count columns, do note that top pad_count rows are already
     // filled
-    blaze::submatrix(
-        result, pad_count, source.columns(), vertical_block_height, pad_count) =
+    blaze::submatrix(result, pad_count, source.columns(), vertical_block_height, pad_count) =
         padding_value;
 
     // don't forget to copy the original contents
-    blaze::submatrix(
-        result, pad_count, pad_count, source.rows(), source.columns()) = source;
+    blaze::submatrix(result, pad_count, pad_count, source.rows(), source.columns()) = source;
 
     return result;
 }
