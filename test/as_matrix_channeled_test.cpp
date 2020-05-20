30b5a3a6545808b4081eef3e0d084c60aea4a53d
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-05-21 04:15:27 +0600

    Add extensive tests to as_matrix_channeled

--- test/as_matrix_channeled_test.cpp
+++ test/as_matrix_channeled_test.cpp
@@ -12,64 +12,92 @@ namespace gil = boost::gil;
 
 gil::rgba8_pixel_t rgba8_zero_pixel(0, 0, 0, 0);
 
-TEST_CASE("rgba8_image_t as matrix typecheck", "[as_matrix_channeled]")
+template <typename PixelType, typename ImageType>
+void pixel4_uint8_zeroes_test()
 {
-    gil::rgba8_image_t image(16, 16, rgba8_zero_pixel);
-    auto view = gil::view(image);
-
-    auto matrix_view = flash::as_matrix_channeled(view);
-    using expected_t = blaze::CustomMatrix<
-        blaze::StaticVector<std::uint8_t, 4, blaze::rowMajor, blaze::unaligned, blaze::unpadded>,
-        blaze::unaligned,
-        blaze::unpadded,
-        blaze::rowMajor>;
-    STATIC_REQUIRE(std::is_same_v<decltype(matrix_view), expected_t>);
-}
-
-TEST_CASE("rgba8_image_t zero as matrix valuecheck", "[as_matrix_channeled]")
-{
-    gil::rgba8_image_t image(16, 16, rgba8_zero_pixel);
+    PixelType zero_pixel = PixelType(0, 0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
     auto view = gil::view(image);
 
     auto matrix_view = flash::as_matrix_channeled(view);
     REQUIRE(blaze::isZero(matrix_view));
 }
 
-TEST_CASE("rgba8_image_t different values as matrix valuecheck", "[as_matrix_channeled]")
+template <typename PixelType, typename ImageType>
+void pixel4_uint8_different_values_test()
 {
-    gil::rgba8_image_t image(16, 16, rgba8_zero_pixel);
+    PixelType zero_pixel = PixelType(0, 0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
     auto view = gil::view(image);
 
-    std::uint8_t value(255);
     auto matrix_view = flash::as_matrix_channeled(view);
+    std::uint8_t value(255);
     for (std::size_t i = 0; i < matrix_view.rows(); ++i) {
         for (std::size_t j = 0; j < matrix_view.columns(); ++j) {
-            matrix_view(i, j) = {static_cast<unsigned char>(i), static_cast<unsigned char>(j), value, 0};
+            matrix_view(i, j) = {
+                static_cast<unsigned char>(i), static_cast<unsigned char>(j), value, 0};
         }
     }
 
     for (flash::signed_size i = 0; i < matrix_view.rows(); ++i) {
         for (flash::signed_size j = 0; j < matrix_view.columns(); ++j) {
-            gil::rgba8_pixel_t expected_pixel(static_cast<unsigned char>(i), static_cast<unsigned char>(j), value, 0);
+            gil::rgba8_pixel_t expected_pixel(
+                static_cast<unsigned char>(i), static_cast<unsigned char>(j), value, 0);
             REQUIRE(view(j, i) == expected_pixel);
         }
     }
 }
 
-gil::rgb32f_pixel_t rgb32f_zero_pixel(0, 0, 0);
-
-TEST_CASE("rgb32f_image_t zero as matrix valuecheck", "[as_matrix_channeled]")
+template <typename PixelType, typename ImageType>
+void pixel4_float32_zeroes_test()
 {
-    gil::rgb32f_image_t image(16, 16, rgb32f_zero_pixel);
+    PixelType zero_pixel = PixelType(0, 0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
     auto view = gil::view(image);
 
     auto matrix_view = flash::as_matrix_channeled(view);
     REQUIRE(blaze::isZero(matrix_view));
 }
 
-TEST_CASE("rgb32f_image_t different values as matrix valuecheck", "[as_matrix_channeled]")
+template <typename PixelType, typename ImageType>
+void pixel4_float32_different_values_test()
 {
-    gil::rgb32f_image_t image(16, 16, rgb32f_zero_pixel);
+    PixelType zero_pixel = PixelType(0, 0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
+    auto view = gil::view(image);
+
+    auto matrix_view = flash::as_matrix_channeled(view);
+    float value = 0.125;
+    for (std::size_t i = 0; i < matrix_view.rows(); ++i) {
+        for (std::size_t j = 0; j < matrix_view.columns(); ++j) {
+            matrix_view(i, j) = {static_cast<float>(i), static_cast<float>(j), value, 0.0f};
+        }
+    }
+
+    for (flash::signed_size i = 0; i < matrix_view.rows(); ++i) {
+        for (flash::signed_size j = 0; j < matrix_view.columns(); ++j) {
+            PixelType expected_pixel(static_cast<float>(i), static_cast<float>(j), value, 0.0f);
+            REQUIRE(view(j, i) == expected_pixel);
+        }
+    }
+}
+
+template <typename PixelType, typename ImageType>
+void pixel3_float32_zeroes_test()
+{
+    PixelType zero_pixel = PixelType(0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
+    auto view = gil::view(image);
+
+    auto matrix_view = flash::as_matrix_channeled(view);
+    REQUIRE(blaze::isZero(matrix_view));
+}
+
+template <typename PixelType, typename ImageType>
+void pixel3_float32_different_values_test()
+{
+    PixelType zero_pixel = PixelType(0, 0, 0);
+    ImageType image(16, 16, zero_pixel);
     auto view = gil::view(image);
 
     auto matrix_view = flash::as_matrix_channeled(view);
@@ -82,8 +110,48 @@ TEST_CASE("rgb32f_image_t different values as matrix valuecheck", "[as_matrix_ch
 
     for (flash::signed_size i = 0; i < matrix_view.rows(); ++i) {
         for (flash::signed_size j = 0; j < matrix_view.columns(); ++j) {
-            gil::rgb32f_pixel_t expected_pixel(static_cast<float>(i), static_cast<float>(j), value);
+            PixelType expected_pixel(static_cast<float>(i), static_cast<float>(j), value);
             REQUIRE(view(j, i) == expected_pixel);
         }
     }
+}
+
+TEST_CASE("check rgba8 as_matrix_channeled", "[as_matrix_channeled]")
+{
+    using pixel_type = gil::rgba8_pixel_t;
+    using image_type = gil::rgba8_image_t;
+    pixel4_uint8_zeroes_test<pixel_type, image_type>();
+    pixel4_uint8_different_values_test<pixel_type, image_type>();
+}
+
+TEST_CASE("check rgb32f as_matrix_channeled", "[as_matrix_channeled]")
+{
+    using pixel_type = gil::rgb32f_pixel_t;
+    using image_type = gil::rgb32f_image_t;
+    pixel3_float32_zeroes_test<pixel_type, image_type>();
+    pixel3_float32_different_values_test<pixel_type, image_type>();
+}
+
+TEST_CASE("check rgba32f as_matrix_channeled", "[as_matrix_channeled]")
+{
+    using pixel_type = gil::rgba32f_pixel_t;
+    using image_type = gil::rgba32f_image_t;
+    pixel4_float32_zeroes_test<pixel_type, image_type>();
+    pixel4_float32_different_values_test<pixel_type, image_type>();
+}
+
+TEST_CASE("check cmyk8 as_matrix_channeled", "[as_matrix_channeled]")
+{
+    using pixel_type = gil::cmyk8_pixel_t;
+    using image_type = gil::cmyk8_image_t;
+    pixel4_uint8_zeroes_test<pixel_type, image_type>();
+    pixel4_uint8_different_values_test<pixel_type, image_type>();
+}
+
+TEST_CASE("check cmyk32f as_matrix_channeled", "[as_matrix_channeled]")
+{
+    using pixel_type = gil::cmyk32f_pixel_t;
+    using image_type = gil::cmyk32f_image_t;
+    pixel4_float32_zeroes_test<pixel_type, image_type>();
+    pixel4_float32_different_values_test<pixel_type, image_type>();
 }
